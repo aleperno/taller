66ec89015c3dede6fab552d22effc356fd997f83
Alejandro Pernin <ale.pernin@gmail.com>
2015-03-22 22:32:47 -0300

    Initial Commit
    +Test Commit
    +Added .gitignore to avoid updating IDEs configuration & cache files

---
+++ main.cpp
@@ -0,0 +1,15 @@
+/*
+ * main.cpp
+ *
+ *  Created on: Mar 15, 2015
+ *      Author: neo
+ */
+#include <SDL.h>
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+int main( int argc, char* args[] )
+{
+	return 0;
+}
