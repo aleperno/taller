e412bb3efa4e88ca31ad49d74f37a7353eb9cb30
seguijoaquin <segui.joaquin@gmail.com>
2015-06-09 14:49:18 -0300

    unsigned int en personaje.h

--- Headers/Personaje.h
+++ Headers/Personaje.h
@@ -55,7 +55,7 @@ class Personaje
 		float _alto_log;
 		CombosPersonaje* combos;
 		vector<string> bufferTeclas;
-		int bufferTeclasSize;
+		unsigned int bufferTeclasSize;
 		Temporizador* bufferTimer;
 		
 		float _pos_y;
